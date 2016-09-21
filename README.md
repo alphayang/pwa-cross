d3c04eb8cdce4ccabeec99c7aee0ef057733e8e0
Alpha Yang <alpha@bestcloud9.com>
2016-09-21 09:30:09 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,11 @@
+# CrosswalkDemo
+A simple project showing how to embed Crosswalk into an Android application.
+
+You can run the project in Android Studio or create an APK with the following command:
+
+`./gradlew assembleRelease`
+
+or
+
+`./gradlew assembleDebug
+
---
+++ app/src/main/java/com/geekdev/android/crosswalkdemo/MainActivity.java
@@ -0,0 +1,50 @@
+package com.geekdev.android.crosswalkdemo;
+
+import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+
+import org.xwalk.core.XWalkView;
+
+
+public class MainActivity extends ActionBarActivity {
+    private XWalkView xWalkWebView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        xWalkWebView=(XWalkView)findViewById(R.id.xwalkWebView);
+        xWalkWebView.load("https://weather-pwa-sample.firebaseapp.com/step-08/", null);
+
+        // turn on debugging
+        //XWalkPreferences.setValue(XWalkPreferences.REMOTE_DEBUGGING, true);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        if (xWalkWebView != null) {
+            xWalkWebView.pauseTimers();
+            xWalkWebView.onHide();
+        }
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        if (xWalkWebView != null) {
+            xWalkWebView.resumeTimers();
+            xWalkWebView.onShow();
+        }
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        if (xWalkWebView != null) {
+            xWalkWebView.onDestroy();
+        }
+    }
+
+}
